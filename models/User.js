0b8c6c81f39da8dadc3e218acf41f95b541f6c5e
JaredIng87 <jaredingersoll14@gmail.com>
2023-01-01 16:27:18 -0500

    add models UNFINISHED

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const { Schema, Types } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    userName: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    thoughts: {
+
+    },
+    friends: {
+
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+module.exports = userSchema;
