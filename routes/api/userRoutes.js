bee304a1a8db0899367edbaf84c17be22401a8bd
JaredIng87 <jaredingersoll14@gmail.com>
2023-01-01 17:02:17 -0500

    update routes: add friends to user routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -4,6 +4,8 @@ const {
   getSingleUser,
   createUser,
   deleteUser,
+  createFriend,
+  deleteFriend,
 } = require('../../controllers/userController');
 
 // /api/students
@@ -11,5 +13,7 @@ router.route('/').get(getUsers).post(createUser);
 
 // /api/students/:studentId
 router.route('/:userId').get(getSingleUser).delete(deleteUser);
+
+router.route('/:userId/friends/:friendId').post(createFriend).delete(deleteFriend);
 
 module.exports = router;
